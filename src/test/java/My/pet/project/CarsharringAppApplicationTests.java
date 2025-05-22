9dfe71a4f59e05329f13b0d7eba29ad094b376b3
Slava_Moiseev <qualityengineerinkom@gmail.com>
2025-05-22 20:31:48 +0300

    Initial commit

---
+++ src/test/java/My/pet/project/CarsharringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package My.pet.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarsharringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
